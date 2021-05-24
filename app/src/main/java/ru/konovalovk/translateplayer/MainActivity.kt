fb1de3092d54c377de099244da3cd74ed6a1c3a5
Konovalov Kirill <konovalov.k.a@yandex.ru>
2021-05-24 15:10:09 +0300

    Init empty proj

---
+++ app/src/main/java/ru/konovalovk/translateplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.konovalovk.translateplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
