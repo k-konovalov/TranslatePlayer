703d11e724947f8b58c26d4e95cbe89b8c9e74db
Konovalov Kirill <konovalov.k.a@yandex.ru>
2021-05-26 11:30:41 +0300

    Link nav controller & appbar

--- app/src/main/java/ru/konovalovk/translateplayer/ui/MainActivity.kt
+++ app/src/main/java/ru/konovalovk/translateplayer/ui/MainActivity.kt
@@ -3,16 +3,25 @@ package ru.konovalovk.translateplayer.ui
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.doOnLayout
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import ru.konovalovk.translateplayer.R
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     val TAG = this::class.java.simpleName
+    private val navController by lazy { findNavController(R.id.flMain) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
+        findViewById<FragmentContainerView>(R.id.flMain).doOnLayout {
+            val appBarConfiguration = AppBarConfiguration(navController.graph)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+        }
     }
 }
